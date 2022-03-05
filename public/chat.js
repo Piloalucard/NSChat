d9b3d4d14218a88a7eb46bb6ea35dad2f4850861
Gustavo P <piloalucard@gmail.com>
2022-03-05 00:25:45 -0600

    NS chat

---
+++ public/chat.js
@@ -0,0 +1,34 @@
+//Make connection
+var socket = io.connect('http://localhost:4000');
+
+//Query DOOM
+var message = document.getElementById('msg');
+var handle = document.getElementById('name');
+var btnSend = document.getElementById('send');
+var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
+
+//Emit events
+btnSend.addEventListener('click',function (){
+    socket.emit('chat',{
+        message: message.value,
+        handle: handle.value
+
+    });
+});
+
+message.addEventListener('keypress',function(){
+    socket.emit('typing',handle.value);
+});
+
+
+
+//Listen for events
+socket.on('chat',function(data){
+    feedback.innerHTML = "";
+    output.innerHTML += "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
+
+socket.on('typing',function(data){
+    feedback.innerHTML += '<p><em>'+data+' is typing a message...</em></p>';
+});
